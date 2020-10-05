d8755dd3b72fa6b34de0c33737ed228bab07f247
Argentina Ortega Sáinz <argen.ortega@gmail.com>
2020-10-06 00:05:37 +0200

    Add export templates
    This commit adds the ability to customize formatting
    and to use templates with placeholders for export
    
    This commit introduces other important changes:
    - Renames the "Export Zotero item" menu to "Export to markdown"
        - The menu to export a Zotero note was merged into "Export to
        markdown"
    - Removes unused preferences that are replaced by placeholders
    - Changes the export when using the single-file option
    - Refactoring of existing methods to match SRP

--- defaults/preferences/mdnotes.js
+++ defaults/preferences/mdnotes.js
@@ -1,48 +1,38 @@
 pref("extensions.mdnotes.directory", "");
 pref("extensions.mdnotes.citekey_title", true);
 pref("extensions.mdnotes.link_style", "wiki");
-pref("extensions.mdnotes.link_dates", true);
-pref("extensions.mdnotes.link_type", true);
 pref("extensions.mdnotes.bullet", "*");
 
-pref("extensions.mdnotes.wildcards.title", '{"line":"# {{field}}", "format": "{{content}}", "link_style": "no-links"}');
-pref("extensions.mdnotes.wildcards.abstract", '{"line":"## Abstract\\n\\n{{field}}\\n", "format": "{{content}}", "link_style": "no-links", "list_separator": ", "}');
-pref("extensions.mdnotes.wildcards.authors", '{"line":"{{bullet}} Authors: {{field}}", "link_style": "wiki", "list_separator": ", "}');
-pref("extensions.mdnotes.wildcards.collections", '{"line":"{{bullet}} Topics: {{field}}", "format": "{{content}}", "link_style": "wiki", "list_separator": ", "}');
-pref("extensions.mdnotes.wildcards.related", '{"line":"{{bullet}} Related: {{field}}", "link_style": "wiki", "list_separator": ", "}');
-pref("extensions.mdnotes.wildcards.notes", '{"line":"## Highlights and Annotations\\n\\n- {{field}}", "format": "{{content}}", "link_style": "wiki", "list_separator": "\\n- "}');
-pref("extensions.mdnotes.wildcards.tags", '{"line":"{{bullet}} Tags: {{field}}", "format": "#{{content}}", "link_style": "no-links", "list_separator": ", ", "remove_spaces": "true"}');
-pref("extensions.mdnotes.wildcards.citekey", '{"line":"{{bullet}} Cite key: {{field}}", "format": "{{content}}", "link_style": "no-links"}');
-pref("extensions.mdnotes.wildcards.pdfs", '{"line":"{{bullet}} PDF Attachments\\n\\t- {{field}}", "format": "{{content}}", "list_separator": "\\n\\t- "}');
-pref("extensions.mdnotes.wildcards.url", '{"line":"{{bullet}} URL: {{field}}", "format": "{{content}}"}');
-pref("extensions.mdnotes.wildcards.doi", '{"line":"{{bullet}} DOI: {{field}}", "format": "{{content}}"}');
+pref("extensions.mdnotes.placeholder.title", '{"content":"# {{field_contents}}", "field_contents": "{{content}}", "link_style": "no-links"}');
+pref("extensions.mdnotes.placeholder.abstractNote", '{"content":"## Abstract\\n\\n{{field_contents}}\\n", "field_contents": "{{content}}", "link_style": "no-links", "list_separator": ", "}');
+pref("extensions.mdnotes.placeholder.author", '{"content":"{{bullet}} Authors: {{field_contents}}", "link_style": "wiki", "list_separator": ", "}');
+pref("extensions.mdnotes.placeholder.collections", '{"content":"{{bullet}} Topics: {{field_contents}}", "field_contents": "{{content}}", "link_style": "wiki", "list_separator": ", "}');
+pref("extensions.mdnotes.placeholder.related", '{"content":"{{bullet}} Related: {{field_contents}}", "link_style": "wiki", "list_separator": ", "}');
+pref("extensions.mdnotes.placeholder.notes", '{"content":"## Highlights and Annotations\\n\\n- {{field_contents}}", "field_contents": "{{content}}", "link_style": "wiki", "list_separator": "\\n- "}');
+pref("extensions.mdnotes.placeholder.tags", '{"content":"{{bullet}} Tags: {{field_contents}}", "field_contents": "#{{content}}", "link_style": "no-links", "list_separator": ", ", "remove_spaces": "true"}');
+pref("extensions.mdnotes.placeholder.citekey", '{"content":"{{bullet}} Cite key: {{field_contents}}", "field_contents": "{{content}}", "link_style": "no-links"}');
+pref("extensions.mdnotes.placeholder.pdfAttachments", '{"content":"{{bullet}} PDF Attachments\\n\\t- {{field_contents}}", "field_contents": "{{content}}", "list_separator": "\\n\\t- "}');
+pref("extensions.mdnotes.placeholder.url", '{"content":"{{bullet}} URL: {{field_contents}}", "field_contents": "{{content}}"}');
+pref("extensions.mdnotes.placeholder.DOI", '{"content":"{{bullet}} DOI: {{field_contents}}", "field_contents": "{{content}}", "link_style": "no-links"}');
+pref("extensions.mdnotes.placeholder.cloudLibrary", '{"content":"{{bullet}} {{field_contents}}"}');
+pref("extensions.mdnotes.placeholder.localLibrary", '{"content":"{{bullet}} {{field_contents}}"}');
+pref("extensions.mdnotes.placeholder.noteContent", '{"content":"{{field_contents}}"}');
 
-
-pref("extensions.mdnotes.export_type", true);
-pref("extensions.mdnotes.export_authors", true);
-pref("extensions.mdnotes.export_dates", true);
-pref("extensions.mdnotes.export_pub_title", true);
-pref("extensions.mdnotes.export_local_library", true);
-pref("extensions.mdnotes.export_cloud_library", true);
-pref("extensions.mdnotes.export_citekey", true);
-pref("extensions.mdnotes.export_urls", true);
-pref("extensions.mdnotes.export_collections", true);
-pref("extensions.mdnotes.export_related", true);
-pref("extensions.mdnotes.export_tags", true);
-pref("extensions.mdnotes.export_pdfs", true);
-pref("extensions.mdnotes.export_abstract", true);
-
-pref("extensions.mdnotes.tag_format", "internal");
-pref("extensions.mdnotes.import_tag", "[[Zotero Import]]");
-
-pref("extensions.mdnotes.export_notes", true);
-pref("extensions.mdnotes.export_notes_heading", "## Highlights and Annotations");
+pref("extensions.mdnotes.templates.directory", "");
+pref("extensions.mdnotes.templates.zotero_metadata", "");
+pref("extensions.mdnotes.templates.zotero_note", "");
+pref("extensions.mdnotes.templates.include_empty_placeholders", false);
 
 pref("extensions.mdnotes.file_conf", "split");
-pref("extensions.mdnotes.title_suffix", "-zotero");
-pref("extensions.mdnotes.notes_suffix", "");
+pref("extensions.mdnotes.files.zotero.metadata_prefix", "Z-");
+pref("extensions.mdnotes.files.zotero.metadata_suffix", "-zotero");
+pref("extensions.mdnotes.files.zotero.note_prefix", "ZN-");
+pref("extensions.mdnotes.files.zotero.note_suffix", "-zotero-note");
+pref("extensions.mdnotes.files.mdnotes.hub_prefix", "MD-");
+pref("extensions.mdnotes.files.mdnotes.hub_suffix", "-mdnotes");
+pref("extensions.mdnotes.files.mdnotes.standalone_prefix", "S-");
+pref("extensions.mdnotes.files.mdnotes.standalone_suffix", "-standalone");
 pref("extensions.mdnotes.create_notes_file", true);
-pref("extensions.mdnotes.obsidian.transclude_metadata", true);
 pref("extensions.mdnotes.attach_to_zotero", true);
 
 pref("extensions.mdnotes.html_to_md", '{"<p>": "", "</p>": "", "<strong>": "**", "</strong>": "**","<b>": "**","</b>": "**","<u>": "#### ","</u>": "","<em>": "*","</em>": "*", "<blockquote>": "> ", "</blockquote>": "", "<br><br>": "\\n\\n"}');
